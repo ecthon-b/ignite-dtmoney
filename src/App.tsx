f32ce5e11730e652ac54172f7518d45bb4475e8c
ecthon <ecthon@gmail.com>
2021-12-22 10:49:42 -0400

    Configurando estilos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>dtamoney</h1>
+      <GlobalStyle />
     </div>
   );
 }
